ef39ffa8f99c29e893c97cbee44e0a59816853c0
Prolanx <prolanxdigital@gmail.com>
2023-11-13 08:00:32 +0100

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "node:path"
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/Prolanx-Frontend/",
+  base: "./Prolanx-Frontend",
   plugins: [react()],
   resolve: {
     alias: {
